c9640acd1f92346770ff4a35052b515846ef8fd3
javiersosabreyer <sosabreyer@gmail.com>
2024-12-26 17:39:48 +0000

    Delete custom hook

--- actividad-1/src/App.js
+++ actividad-1/src/App.js
@@ -1,19 +1,13 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import {Footer} from "./components/Footer";
 import Router from "./routes/Router";
 import {BookContext} from "./context/BookContext";
 import {CartContext} from "./context/CartContext";
 import {useBooks} from "./hooks/useBooks";
-import {useStoredItems} from "./hooks/useStoredItems";
 
 function App() {
     const books = useBooks();
-    const storedItems = useStoredItems();
     const [cartItems, setCartItems] = useState([]);
-
-    useEffect(() => {
-        setCartItems(storedItems);
-    }, [storedItems]);
 
     return (
         <BookContext.Provider value={{books}}>
